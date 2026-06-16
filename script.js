dbb4597859c30a4db293fe63b7aaa0d5d8fcf423
Murray Booth <murraystuartbooth@gmail.com>
2026-06-16 13:49:58 +1000

    Make cursor cloud obvious for testing

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ if (cursorCloud && window.matchMedia("(pointer: fine)").matches && !window.match
   function animateCloud() {
     currentX += (targetX - currentX) * 0.12;
     currentY += (targetY - currentY) * 0.12;
-    cursorCloud.style.transform = `translate3d(${currentX - 150}px, ${currentY - 150}px, 0)`;
+    cursorCloud.style.transform = `translate3d(${currentX - 210}px, ${currentY - 210}px, 0)`;
     requestAnimationFrame(animateCloud);
   }
 
